35bfc5759d8ddf956e5d038f0c24f07f618f2c39
AishaRafeeq <aisharafeeq15@gmail.com>
2025-09-02 03:04:18 +0530

    Add initial migrations

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from django.contrib.auth import get_user_model
+
 
 from pathlib import Path
 from datetime import timedelta
@@ -178,14 +178,5 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "https://*.up.railway.app",
 ]
-User = get_user_model()
-
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin123")
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser created ✅")
 
 print("DEBUG MODE:", DEBUG)
