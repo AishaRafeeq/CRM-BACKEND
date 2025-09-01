132a367079c3953e26e9b0c181e1ed6670a4d903
AishaRafeeq <aisharafeeq15@gmail.com>
2025-09-01 22:53:24 +0530

    Configure database for Railway

--- backend/settings.py
+++ backend/settings.py
@@ -91,8 +91,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+    "default": dj_database_url.config(
+        default=os.getenv("postgresql://postgres:iQxGAIFqGFYWkDfgTjMKhBMqaDfjYAXy@yamanote.proxy.rlwy.net:16364/railway"),
+        conn_max_age=600,  # keep connections alive for performance
     )
 }
 
