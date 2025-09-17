c4ad51a3cb42dec6345f816eea5e509a168d8023
AishaRafeeq <aisharafeeq15@gmail.com>
2025-09-17 18:41:54 +0530

    deployment changes

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,8 @@ ALLOWED_HOSTS = ["*"]  # For production, replace with your domains
 # CORS & CSRF settings
 INSTALLED_NETLIFY_FRONTENDS = [
     "https://kaleidoscopic-bunny-b8322e.netlify.app",  # Add other Netlify URLs if needed
-    "https://frabjous-seahorse-668c7b.netlify.app",
-    "https://subtle-platypus-1d4fd8.netlify.app"
-    "https://car-selling-crm.netlify.app"
+    
+    "https://car-selling-crm.netlify.app",
 ]
 
 CORS_ALLOWED_ORIGINS = [
