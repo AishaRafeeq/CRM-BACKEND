ca34657a7be438fc2b688a21665205855a302b66
AishaRafeeq <aisharafeeq15@gmail.com>
2025-09-17 17:51:50 +0530

    deployment changes

--- backend/settings.py
+++ backend/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = ["*"]  # For production, replace with your domains
 INSTALLED_NETLIFY_FRONTENDS = [
     "https://kaleidoscopic-bunny-b8322e.netlify.app",  # Add other Netlify URLs if needed
     "https://frabjous-seahorse-668c7b.netlify.app",
+    "https://subtle-platypus-1d4fd8.netlify.app"
 ]
 
 CORS_ALLOWED_ORIGINS = [
