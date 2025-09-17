5804ff20607ea11ccf64125dea5b57b375526c62
AishaRafeeq <aisharafeeq15@gmail.com>
2025-09-17 18:59:49 +0530

    deployment changes

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = ["*"]  # For production, replace with your domains
 # CORS & CSRF settings
 INSTALLED_NETLIFY_FRONTENDS = [
     "https://kaleidoscopic-bunny-b8322e.netlify.app",  # Add other Netlify URLs if needed
-    
+    "https://crm-frontend-car.onrender.com",
     "https://car-selling-crm.netlify.app",
 ]
 
